b1c9bc483a74afde280d79f42fc735e214fb42f4
virginiah894 <virgyperry@gmail.com>
2020-01-21 16:58:37 +0300

    initial commit

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/moringa/Documents/MORINGA PROJECTS/Capstone Job Link/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
