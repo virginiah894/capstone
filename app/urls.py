26c798d60c7b0ee3d9aacd6aa0a523068d8dc727
virginiah894 <virgyperry@gmail.com>
2020-01-22 10:58:25 +0300

    add different paths to the page

---
+++ app/urls.py
@@ -0,0 +1,18 @@
+
+from django.urls import path , include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+
+
+    path('', views.home,name='home'),
+    path('profile/', views.profile , name = 'profile'),
+    path('update_profile/',views.update_profile,name='update'),
+
+]
+
+if settings.DEBUG:
+  urlpatterns+= static( settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
