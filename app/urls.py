0d3edda0adb8d83233d1af2f51b6c93bf433d233
virginiah894 <virgyperry@gmail.com>
2020-01-23 16:53:46 +0300

    Adds more paths for creating ,updating and details view

--- app/urls.py
+++ app/urls.py
@@ -6,11 +6,17 @@ from django.conf.urls.static import static
 from .views import *
 
 
+
 urlpatterns = [
 
 
-    path('', views.home,name='home'),
+    path('', PostListView.as_view(),name='home'),
+    path('post/new/', PostCreateView.as_view(),name='post-create'),
+    path('job/<int:pk>/',PostDetailView.as_view(),name='job-detail'),
     path('profile/', views.profile , name = 'profile'),
+    
+
+
     path('update_profile/',views.update_profile,name='update'),
     path('jobs', views.jobs, name='jobs'),
     path('new/job', views.new_job, name='newJob'),
