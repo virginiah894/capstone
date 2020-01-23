c8069003fd349a5162c3af538dbef85d159e3eb4
virginiah894 <virgyperry@gmail.com>
2020-01-23 16:56:17 +0300

    updates and adds more routes

--- joblink/urls.py
+++ joblink/urls.py
@@ -6,17 +6,12 @@ from app.views import *
 
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('social_django.urls',namespace = 'social')),
     path('accounts/', include('registration.backends.simple.urls')),
     path('',include('app.urls')),
-    path('accounts/signup/', SignUpView.as_view(), name='signup'),
- 
-    path('accounts/signup/basicuser/', BasicSignUpView.as_view(), name='basicuser'),
-    
-    path('accounts/signup/jobseeker/', JobSignUpView.as_view(), name='jobseeker'),
-    path('accounts/signup/employer/', EmployerSignUpView.as_view(), name='employer'),
 
     path('logout/', auth_views.LogoutView, {"next_page": '/'}),
 ]
