b219f654ccc45f248e7f86181b209dea3689e20f
virginiah894 <virgyperry@gmail.com>
2020-01-23 16:52:25 +0300

    removes all unnecessary signupforms

--- app/forms.py
+++ app/forms.py
@@ -4,54 +4,6 @@ from django  import forms
 from crispy_forms.helper import FormHelper
 from  crispy_forms.layout import Submit,Layout,Field
 
-from django.db import transaction
-from app.models import *
-
-
-
-    
-class JobSignUpForm(UserCreationForm):
-    interested_jobs = forms.ModelMultipleChoiceField(
-        queryset=Subject.objects.all(),
-        widget=forms.CheckboxSelectMultiple,
-        required=True
-    )
-
-    class Meta(UserCreationForm.Meta):
-        model = User
-
-    @transaction.atomic
-    def save(self):
-        user = super().save(commit=False)
-        user.is_jobseeker = True
-        user.save()
-        jobseeker = User.objects.create(user=user)
-        jobseeker.interests.add(*self.cleaned_data.get('interested_jobs'))
-        return user
-
-
-class EmployerSignUpForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        model = User
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.is_employer = True
-        if commit:
-            user.save()
-        return user
-
-
-class BasicSignUpForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        model = User
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.is_basic = True
-        if commit:
-            user.save()
-        return user
 
 
 
