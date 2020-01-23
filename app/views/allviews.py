ffa9e947d25036d193a91c1c9de1f4200ac4c2fe
virginiah894 <virgyperry@gmail.com>
2020-01-23 08:45:33 +0300

    create diffrent views for different users

---
+++ app/views/allviews.py
@@ -0,0 +1,14 @@
+from django.shortcuts import redirect, render
+from django.views.generic import TemplateView
+
+class SignUpView(TemplateView):
+    template_name = 'registration/signup.html'
+
+
+def home(request):
+    if request.user.is_authenticated:
+        if request.user.is_jobseeker:
+            return redirect('/')
+        else:
+            return redirect('/')
+    return render(request, '/')
